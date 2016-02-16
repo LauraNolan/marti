dc9be1d20882d633da908816a290e67b238d4ef0
Zaza Soriano <zaza.soriano@jhuapl.edu>
2016-02-16 12:11:00 -0500

    made kill chain LMCO constants

--- crits/vocabulary/kill_chain.py
+++ crits/vocabulary/kill_chain.py
@@ -1,14 +1,15 @@
 from crits.vocabulary.vocab import vocab
+import stix.common.kill_chains.lmco as lmco
 
 class KillChain(vocab):
     """
     Vocabulary for Kill Chain.
     """
 
-    RECON = "Reconnaissance"
-    WEAPONIZE = "Weaponization"
-    DELIVER = "Delivery"
-    EXPLOIT = "Exploitation"
-    INSTALL = "Installation"
-    CONTROL = "Command and Control"
-    ACTIONS = "Actions on Objectives"
+    RECON = str(lmco.PHASE_RECONNAISSANCE.ordinality) + ". " + lmco.PHASE_RECONNAISSANCE.name
+    WEAPONIZE = str(lmco.PHASE_WEAPONIZE.ordinality) + ". " + lmco.PHASE_WEAPONIZE.name
+    DELIVER = str(lmco.PHASE_DELIVERY.ordinality) + ". " + lmco.PHASE_DELIVERY.name
+    EXPLOIT = str(lmco.PHASE_EXPLOITATION.ordinality) + ". " + lmco.PHASE_EXPLOITATION.name
+    INSTALL = str(lmco.PHASE_INSTALLATION.ordinality) + ". " + lmco.PHASE_INSTALLATION.name
+    CONTROL = str(lmco.PHASE_COMMAND_AND_CONTROL.ordinality) + ". " + lmco.PHASE_COMMAND_AND_CONTROL.name
+    ACTIONS = str(lmco.PHASE_ACTIONS_AND_OBJECTIVES.ordinality) + ". " + lmco.PHASE_ACTIONS_AND_OBJECTIVES.name
